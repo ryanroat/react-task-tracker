ff107f62afd17ac16c57bb7e2a749578ca069662
Ryan Roat <ryanroat@gmail.com>
2021-06-16 11:51:13 -0400

    moved tasks array to global state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 const name = 'Ryan'
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: 'Fathers Day prep',
+        day: 'Jun 16 at 12:00 pm',
+        reminder: false,
+      },
+      {
+        id: 2,
+        text: 'Oscar Dr appointment',
+        day: 'Jun 17 at 8:30 am',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Ryan interview',
+        day: 'Jun 18 at 9:00 am',
+        reminder: true,
+      }
+    ]
+  )
+
   return (
     <div className="container">
       <Header greeting='Welcome' name={name}/>
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,9 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Task 1',
-    day: 'Jun 16 at 4:00 pm',
-    reminder: false,
-  },
-  {
-    id: 2,
-    text: 'Oscar Dr appointment',
-    day: 'Jun 17 at 8:30 am',
-    reminder: true,
-  },
-  {
-    id: 1,
-    text: 'Ryan interview',
-    day: 'Jun 18 at 9:00 am',
-    reminder: true,
-  }
-]
+const Tasks = ({ tasks }) => {
 
-const Tasks = () => {
   return (
     <>
       {tasks.map((task) => (
-        <h3>{task.text}</h3>
+        <h3 key={task.id}>{task.text}</h3>
       ))}
     </>
   )
